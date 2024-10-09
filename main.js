33de93e7ab1a201a229682bcb464f9a08fc8685e
tonygeorge13 <tonygeorge1312004@gmail.com>
2024-10-10 01:49:12 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// window.addEventListener("scroll" , () => {
+// if ( window.scrollY > 100) {
+//     header.classlist.add("scroll-down")
+// } else {
+//     header.classlist.remove("scroll-down")
+// }
+// })
+
+
+
+
+
+
+// window.addEventListener("scroll", () => {
+//     if (window.scrollY > 100) {
+//         header.classList.add("scroll-down")
+//     } else {
+//         header.classList.remove("scroll-down")
+//     }
+//   })
+
+
+
+
+
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      headerElement.classList.add("scroll-down")
+    } else {
+      headerElement.classList.remove("scroll-down")
+    }
+  })
+  
